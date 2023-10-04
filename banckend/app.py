c83dc9a6d8e1f0b1da4896aa11c6fbd5085788f5
tsimagnova <tsimagnova@gmail.com>
2023-10-04 17:13:07 +0300

    first2

--- banckend/app.py
+++ banckend/app.py
@@ -3,4 +3,12 @@ from database import db_connection
 
 app = FastAPI()
 
+@app.get("liste/")
+async def read_root():
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM etudiants")
+    result = cursor.fetchall()
+    cursor.close()
+    return {"message": "Connexion réussie à la base de données RDS", "data": result}
+
 
